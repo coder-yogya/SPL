fd37a749e6ff687ebe890575b32740585e18a977
coder-yogya <yogya.coder@gmail.com>
2025-05-21 15:00:23 +0530

    dev

--- software/main.py
+++ software/main.py
@@ -2,11 +2,9 @@ from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis")
 
+# Analyzing sentiments . (test)
 print(classifier("I love this movie so so so so much"))
-
 print(classifier("I hate this movie"))
-
 print(classifier("I am happy"))
-
 print(classifier("I am sad"))
 
