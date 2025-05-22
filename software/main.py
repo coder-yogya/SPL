728206694e894d2cb59fe189e8bc4cd422151460
coder-yogya <yogya.coder@gmail.com>
2025-05-22 12:54:05 +0530

    dev

--- software/main.py
+++ software/main.py
@@ -4,22 +4,10 @@ import pandas as pd
 data = {
     "name": ["JetBrainsAI","gemini","chatgpt"],
     "license": ["paid","free/paid","free/paid"],
-    "smartness": ["ultimate","basic","ultraUltimate"]
+    "smartness": ["ultimate","basic","ultraUltimate"],
+    "familierness": ["some","no","extreme ❤️"]
 }
 
 df = pd.DataFrame(data)
 
 print(df)
-
-# Transformers
-
-from transformers import pipeline
-
-sentiment_pipeline = pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english"
-)
-
-result = sentiment_pipeline("I love using Jetbrians Fleet code editor. It's amazing! , It's extraordinary!")
-
-print(f"Sentiment: {result}")
